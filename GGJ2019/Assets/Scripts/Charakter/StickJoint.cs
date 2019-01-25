8126f8e13cb3a049be80c4a2c447e82f96e084bc
Benedikt Zoeller <sephirot5@web.de>
2019-01-25 23:30:45 +0100

    Added JointFunction to Character-Sticks

---
+++ GGJ2019/Assets/Scripts/Charakter/StickJoint.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Charakter
+{
+    class StickJoint: MonoBehaviour
+    {
+        #region Variables
+
+
+
+        #region Serializefields
+
+        [SerializeField]
+        Transform topJoint, bottomJoint;
+
+        [SerializeField]
+        float bottomJointDistance;
+
+        #endregion
+
+        #endregion
+
+        #region Properties
+
+
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Update()
+        {
+            if(topJoint && bottomJoint)
+            {
+                transform.position = topJoint.position * 0.5f + (bottomJoint.position + Vector3.right * bottomJointDistance) * 0.5f;
+                transform.forward = topJoint.position - (bottomJoint.position + Vector3.right * bottomJointDistance);
+                transform.Rotate(Vector3.right, 90);
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+
+
+        #endregion
+
+        #region Private Methods
+
+
+
+        #endregion
+
+        #region Structs
+
+
+
+        #endregion
+
+        #region Enums
+
+
+
+        #endregion
+    }
+}
