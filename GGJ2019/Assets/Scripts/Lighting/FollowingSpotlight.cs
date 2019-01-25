8f4b6b9ffbdefccbf1c5390d084d4f3104a18806
Benedikt Zoeller <sephirot5@web.de>
2019-01-25 19:23:52 +0100

    Added Spotlight-Following Script or easier Light-Animation

---
+++ GGJ2019/Assets/Scripts/Lighting/FollowingSpotlight.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEditor;
+
+namespace GGJ.Lighting
+{
+    class FollowingSpotlight : MonoBehaviour
+    {
+        #region Variables
+
+
+
+        #region Serializefields
+
+        [SerializeField]
+        Light spotlight;
+
+        [SerializeField]
+        Transform lookAt;
+
+        #endregion
+
+        #endregion
+
+        #region Properties
+
+
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Awake()
+        {
+            if(!spotlight)
+            {
+                spotlight = GetComponent<Light>();
+            }
+        }
+
+        private void Update()
+        {
+            if(lookAt && spotlight)
+            {
+                spotlight.transform.LookAt(lookAt);
+            }
+            else
+            {
+                Debug.Log("No Object to look at found");
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+
+
+        #endregion
+
+        #region Private Methods
+
+
+
+        #endregion
+
+        #region Structs
+
+
+
+        #endregion
+
+        #region Enums
+
+
+
+        #endregion
+    }
+}
